92620b0151c34d66d751da2b88272f9000cccbb5
Owain Lewis <owain@owainlewis.com>
2017-10-30 15:55:27 +0000

    Tidy up work on release script

--- hack/release.sh
+++ hack/release.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Edit this to perform a new release
 RELEASE=0.1.0
+
+IMAGE=wcr.io/oracle/oci-cloud-controller-manager
 
 if git rev-parse "$RELEASE" >/dev/null 2>&1; then
     echo "Tag $RELEASE already exists. Doing nothing"
 else
     SHA=$(git rev-parse --short=8 HEAD)
+
     echo "Creating new release $RELEASE for SHA $SHA"
+
     git tag -a "$RELEASE" -m "Release version: $RELEASE"
     git push --tags
-    docker pull wcr.io/oracle/oci-cloud-controller-manager:$SHA
-    docker tag wcr.io/oracle/oci-cloud-controller-manager:$SHA wcr.io/oracle/oci-cloud-controller-manager:$RELEASE
-    docker push wcr.io/oracle/oci-cloud-controller-manager:$RELEASE
+
+    docker pull $IMAGE:$SHA
+    docker tag $IMAGE:$SHA $IMAGE:$RELEASE
+    docker push $IMAGE:$RELEASE
 fi
