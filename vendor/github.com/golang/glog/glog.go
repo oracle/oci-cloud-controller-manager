02e6b01fabb6967d9012766dfb174e08732cf409
Andrew Pryde <andrew.pryde@oracle.com>
2018-08-09 11:11:26 +0100

    Replace vendored glog

--- vendor/github.com/golang/glog/glog.go
+++ vendor/github.com/golang/glog/glog.go
@@ -21,14 +21,74 @@
 package glog
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strconv"
+	"sync"
+	"sync/atomic"
 
 	"go.uber.org/zap"
 )
 
-// Level is a shim
+// loggingT collects all the global state of the logging setup.
+type loggingT struct {
+	// mu protects the remaining elements of this structure
+	mu        sync.Mutex
+	verbosity Level // V logging level, the value of the -v flag/
+}
+
+var logging loggingT
+
+// Level is exported because it appears in the arguments to V and is
+// the type of the v flag, which can be set programmatically.
+// It's a distinct type because we want to discriminate it from logType.
+// Variables of type level are only changed under logging.mu.
+// The -v flag is read only with atomic ops, so the state of the logging
+// module is consistent.
+
+// Level is treated as a sync/atomic int32.
+
+// Level specifies a level of verbosity for V logs. *Level implements
+// flag.Value; the -v flag is of type Level and should be modified
+// only through the flag.Value interface.
 type Level int32
+
+// get returns the value of the Level.
+func (l *Level) get() Level {
+	return Level(atomic.LoadInt32((*int32)(l)))
+}
+
+// set sets the value of the Level.
+func (l *Level) set(val Level) {
+	atomic.StoreInt32((*int32)(l), int32(val))
+}
+
+// String is part of the flag.Value interface.
+func (l *Level) String() string {
+	return strconv.FormatInt(int64(*l), 10)
+}
+
+// Get is part of the flag.Value interface.
+func (l *Level) Get() interface{} {
+	return *l
+}
+
+// Set is part of the flag.Value interface.
+func (l *Level) Set(value string) error {
+	v, err := strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+	logging.mu.Lock()
+	defer logging.mu.Unlock()
+	logging.verbosity.set(Level(v))
+	return nil
+}
+
+func init() {
+	flag.Var(&logging.verbosity, "v", "log level for V logs")
+}
 
 // Verbose is a shim
 type Verbose bool
@@ -38,136 +98,167 @@ func Flush() {
 	zap.L().Sync()
 }
 
-// V is a shim
+// skipLogger constructs a suggared logger with an additional caller skip to
+// ensure we log the correct line number.
+func skipLogger() *zap.SugaredLogger {
+	return zap.L().WithOptions(zap.AddCallerSkip(1)).Sugar()
+}
+
+// max calculates the maximum of the two given Levels.
+func max(a, b Level) Level {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// V reports whether verbosity at the call site is at least the requested level.
+// The returned value is a boolean of type Verbose, which implements Info, Infoln
+// and Infof. These methods will write to the Info log if called.
+//
+// Verbosity is controlled both by the -v flag and the zap log level.
 func V(level Level) Verbose {
-	return Verbose(zap.L().Core().Enabled(zap.DebugLevel))
+	lvl := logging.verbosity.get()
+	core := zap.L().Core()
+	if core.Enabled(zap.DebugLevel) {
+		return Verbose(level <= max(Level(4), lvl))
+	}
+	if core.Enabled(zap.InfoLevel) {
+		return Verbose(level <= max(Level(2), lvl))
+	}
+	return Verbose(level <= lvl)
 }
 
 // Info is a shim
 func (v Verbose) Info(args ...interface{}) {
-	zap.S().Debug(args...)
+	if v {
+		skipLogger().Info(args...)
+	}
 }
 
 // Infoln is a shim
 func (v Verbose) Infoln(args ...interface{}) {
-	s := fmt.Sprint(args)
-	zap.S().Debug(s, "\n")
+	if v {
+		skipLogger().Info(fmt.Sprint(args), "\n")
+	}
 }
 
 // Infof is a shim
 func (v Verbose) Infof(format string, args ...interface{}) {
-	zap.S().Debugf(format, args...)
+	if v {
+		skipLogger().Infof(format, args...)
+	}
 }
 
 // Info is a shim
 func Info(args ...interface{}) {
-	zap.S().Info(args...)
+	skipLogger().Info(args...)
 }
 
 // InfoDepth is a shim
 func InfoDepth(depth int, args ...interface{}) {
-	zap.S().Info(args...)
+	skipLogger().Info(args...)
 }
 
 // Infoln is a shim
 func Infoln(args ...interface{}) {
 	s := fmt.Sprint(args)
-	zap.S().Info(s, "\n")
+	skipLogger().Info(s, "\n")
 }
 
 // Infof is a shim
 func Infof(format string, args ...interface{}) {
-	zap.S().Infof(format, args...)
+	skipLogger().Infof(format, args...)
 }
 
 // Warning is a shim
 func Warning(args ...interface{}) {
-	zap.S().Warn(args...)
+	skipLogger().Warn(args...)
 }
 
 // WarningDepth is a shim
 func WarningDepth(depth int, args ...interface{}) {
-	zap.S().Warn(args...)
+	skipLogger().Warn(args...)
 }
 
 // Warningln is a shim
 func Warningln(args ...interface{}) {
 	s := fmt.Sprint(args)
-	zap.S().Warn(s, "\n")
+	skipLogger().Warn(s, "\n")
 }
 
 // Warningf is a shim
 func Warningf(format string, args ...interface{}) {
-	zap.S().Warnf(format, args...)
+	skipLogger().Warnf(format, args...)
 }
 
 // Error is a shim
 func Error(args ...interface{}) {
-	zap.S().Error(args...)
+	skipLogger().Error(args...)
 }
 
 // ErrorDepth is a shim
 func ErrorDepth(depth int, args ...interface{}) {
-	zap.S().Error(args...)
+	skipLogger().Error(args...)
 }
 
 // Errorln is a shim
 func Errorln(args ...interface{}) {
 	s := fmt.Sprint(args)
-	zap.S().Error(s, "\n")
+	skipLogger().Error(s, "\n")
 }
 
 // Errorf is a shim
 func Errorf(format string, args ...interface{}) {
-	zap.S().Errorf(format, args...)
+	skipLogger().Errorf(format, args...)
 }
 
 // Fatal is a shim
 func Fatal(args ...interface{}) {
-	zap.S().Error(args...)
+	skipLogger().Error(args...)
 	os.Exit(255)
 }
 
 // FatalDepth is a shim
 func FatalDepth(depth int, args ...interface{}) {
-	zap.S().Error(args...)
+	skipLogger().Error(args...)
 	os.Exit(255)
 }
 
 // Fatalln is a shim
 func Fatalln(args ...interface{}) {
 	s := fmt.Sprint(args)
-	zap.S().Error(s, "\n")
+	skipLogger().Error(s, "\n")
 	os.Exit(255)
 }
 
 // Fatalf is a shim
 func Fatalf(format string, args ...interface{}) {
-	zap.S().Errorf(format, args...)
+	skipLogger().Errorf(format, args...)
 	os.Exit(255)
 }
 
 // Exit is a shim
 func Exit(args ...interface{}) {
-	zap.S().Error(args...)
+	skipLogger().Error(args...)
 	os.Exit(1)
 }
 
 // ExitDepth is a shim
 func ExitDepth(depth int, args ...interface{}) {
-	zap.S().Error(args...)
+	skipLogger().Error(args...)
 	os.Exit(1)
 }
 
 // Exitln is a shim
 func Exitln(args ...interface{}) {
 	s := fmt.Sprint(args)
-	zap.S().Error(s, "\n")
+	skipLogger().Error(s, "\n")
 	os.Exit(1)
 }
 
 // Exitf is a shim
 func Exitf(format string, args ...interface{}) {
-	zap.S().Errorf(format, args...)
+	skipLogger().Errorf(format, args...)
 	os.Exit(1)
 }
