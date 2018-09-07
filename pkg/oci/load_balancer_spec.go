b74094b2ecae751d00226219b897f03091ce1085
MadalinaPatrichi <ioana-madalina.patrichi@oracle.com>
2018-09-07 14:25:52 +0100

    Restore ca

--- pkg/oci/load_balancer_spec.go
+++ pkg/oci/load_balancer_spec.go
@@ -161,7 +161,7 @@ func (s *LBSpec) Certificates() (map[string]loadbalancer.CertificateDetails, err
 		return certs, nil
 	}
 
-	_, cert, key, pass, err := s.SSLConfig.readSSLSecret(s.service)
+	cacert, cert, key, pass, err := s.SSLConfig.readSSLSecret(s.service)
 	if err != nil {
 		return nil, errors.Wrap(err, "reading SSL Secret")
 	}
@@ -172,7 +172,7 @@ func (s *LBSpec) Certificates() (map[string]loadbalancer.CertificateDetails, err
 	certs[s.SSLConfig.Name] = loadbalancer.CertificateDetails{
 		CertificateName:   &s.SSLConfig.Name,
 		PublicCertificate: &cert,
-		CaCertificate:     common.String("fsfsdfdfs"),
+		CaCertificate:     &cacert,
 		PrivateKey:        &key,
 		Passphrase:        &pass,
 	}
