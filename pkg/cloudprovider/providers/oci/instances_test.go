5996103c9ba7dbc766843039b40835ad1cfd96b7
arindam-bandyopadhyay <arindam.bandyopadhyay@oracle.com>
2020-06-30 15:53:52 +0530

    OKE-10509 - CCM should set node address properly to cross compartment node OKE-7743 - Fault Domain Label to the Nodes

--- pkg/cloudprovider/providers/oci/instances_test.go
+++ pkg/cloudprovider/providers/oci/instances_test.go
@@ -21,6 +21,9 @@ import (
 	"testing"
 	"time"
 
+	"k8s.io/apimachinery/pkg/labels"
+	listersv1 "k8s.io/client-go/listers/core/v1"
+
 	providercfg "github.com/oracle/oci-cloud-controller-manager/pkg/cloudprovider/providers/oci/config"
 	"github.com/oracle/oci-cloud-controller-manager/pkg/oci/client"
 	"github.com/oracle/oci-go-sdk/common"
@@ -135,7 +138,12 @@ var (
 			Id: common.String("vcnwithoutdnslabel"),
 		},
 	}
+	comportmentID = "xxxx"
 )
+
+
+
+
 
 type MockOCIClient struct{}
 
@@ -167,7 +175,9 @@ func (MockOCIClient) Identity() client.IdentityInterface {
 type MockComputeClient struct{}
 
 func (MockComputeClient) GetInstance(ctx context.Context, id string) (*core.Instance, error) {
-	return nil, nil
+	return &core.Instance{
+		Id: &id,
+	}, nil
 }
 
 func (MockComputeClient) GetInstanceByNodeName(ctx context.Context, compartmentID, vcnID, nodeName string) (*core.Instance, error) {
@@ -374,6 +384,44 @@ func (MockIdentityClient) ListAvailabilityDomains(ctx context.Context, compartme
 	return nil, nil
 }
 
+type mockInstanceCache struct{}
+
+func (m mockInstanceCache) Add(obj interface{}) error {
+	return nil
+}
+
+func (m mockInstanceCache) Update(obj interface{}) error {
+	return nil
+}
+
+func (m mockInstanceCache) Delete(obj interface{}) error {
+	return nil
+}
+
+func (m mockInstanceCache) List() []interface{} {
+	return nil
+}
+
+func (m mockInstanceCache) ListKeys() []string {
+	return nil
+}
+
+func (m mockInstanceCache) Get(obj interface{}) (item interface{}, exists bool, err error) {
+	return &core.Instance{CompartmentId: &comportmentID}, true, nil
+}
+
+func (m mockInstanceCache) GetByKey(key string) (item interface{}, exists bool, err error) {
+	return &core.Instance{CompartmentId: &comportmentID}, true, nil
+}
+
+func (m mockInstanceCache) Replace(i []interface{}, s string) error {
+	return nil
+}
+
+func (m mockInstanceCache) Resync() error {
+	return nil
+}
+
 func TestExtractNodeAddresses(t *testing.T) {
 	testCases := []struct {
 		name string
@@ -454,8 +502,10 @@ func TestExtractNodeAddresses(t *testing.T) {
 	}
 
 	cp := &CloudProvider{
-		client: MockOCIClient{},
-		config: &providercfg.Config{CompartmentID: "testCompartment"},
+		client:        MockOCIClient{},
+		config:        &providercfg.Config{CompartmentID: "testCompartment"},
+		NodeLister:    &mockNodeLister{},
+		instanceCache: &mockInstanceCache{},
 	}
 
 	for _, tt := range testCases {
@@ -469,4 +519,19 @@ func TestExtractNodeAddresses(t *testing.T) {
 			}
 		})
 	}
+}
+
+type mockNodeLister struct{}
+
+func (s *mockNodeLister) List(selector labels.Selector) (ret []*v1.Node, err error) {
+
+	return nil, nil
+}
+
+func (s *mockNodeLister) Get(name string) (*v1.Node, error) {
+	return nil, nil
+}
+
+func (l *mockNodeLister) ListWithPredicate(predicate listersv1.NodeConditionPredicate) ([]*v1.Node, error) {
+	return nil, nil
 }
