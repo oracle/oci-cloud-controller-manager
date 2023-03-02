3b980cefaad91adb57fd2ca2ad5302a4f246ce49
ypgohoka <yashwant.p.gohokar@oracle.com>
2023-03-02 19:12:10 +0530

    Added copyright boilerplate and third_party_license.txt

--- pkg/csi-util/utils.go
+++ pkg/csi-util/utils.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Oracle and/or its affiliates. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package csi_util
 
 import (
@@ -57,7 +71,7 @@ const (
 	RPM_COMMAND                    = "rpm"
 )
 
-//Util interface
+// Util interface
 type Util struct {
 	Logger *zap.SugaredLogger
 }
@@ -167,7 +181,7 @@ func ExtractISCSIInformation(attributes map[string]string) (*disk.Disk, error) {
 	}, nil
 }
 
-//Extracts the vpusPerGB as int64 from given string input
+// Extracts the vpusPerGB as int64 from given string input
 func ExtractBlockVolumePerformanceLevel(attribute string) (int64, error) {
 	vpusPerGB, err := strconv.ParseInt(attribute, 10, 64)
 	if err != nil {
