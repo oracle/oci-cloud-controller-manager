febb7611f306c44c84ddf1498f390ee0b4be5801
yashg <yashas.g.g@oracle.com>
2023-11-23 13:04:02 +0530

    Changes to parallelise e2e test runs

--- test/e2e/cloud-provider-oci/setup.go
+++ test/e2e/cloud-provider-oci/setup.go
@@ -1,22 +1,12 @@
-// Copyright 2020 Oracle and/or its affiliates. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package e2e
 
 import (
+	"time"
+
 	"github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	sharedfw "github.com/oracle/oci-cloud-controller-manager/test/e2e/framework"
+	oke "github.com/oracle/oci-go-sdk/v65/containerengine"
 )
 
 var setupF *sharedfw.Framework
@@ -28,9 +18,12 @@ var _ = ginkgo.SynchronizedBeforeSuite(func() []byte {
 	sharedfw.Logf("CloudProviderFramework Setup")
 	sharedfw.Logf("Running tests with existing cluster.")
 	return nil
-}, func(data []byte) {})
+	}, func(data []byte) {
+		setupF = sharedfw.New()
+	},
+)
 
-var _ = ginkgo.SynchronizedAfterSuite(func() {
+var _ = ginkgo.SynchronizedAfterSuite(func() {}, func() {
 	sharedfw.Logf("Running AfterSuite actions on all node")
 	sharedfw.RunCleanupActions()
-}, func() {})
+})
