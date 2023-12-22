f60fa598c85c2e8aab98fd54a7c9bec7e13709fa
yashg <yashas.g.g@oracle.com>
2023-12-22 20:17:18 +0530

    Changed timeout for CSI ControllerPublish/ControllerUnpublish/ControllerExpand calls

--- pkg/oci/client/volume_attachment.go
+++ pkg/oci/client/volume_attachment.go
@@ -24,7 +24,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
-const attachmentPollInterval = 5 * time.Second
+const attachmentPollInterval = 10 * time.Second
 
 // VolumeAttachmentInterface defines the interface to the OCI volume attachement
 // API.
