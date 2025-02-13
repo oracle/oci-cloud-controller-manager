9cdc5b8b48cb96b02916af8e826e62eae10ff757
Yashwant Gohokar <yashwant.p.gohokar@oracle.com>
2025-02-13 10:15:16 +0530

    Manifest, Third Party License and Doc changes

--- hack/existing-standalone-cluster-env-template.sh
+++ hack/existing-standalone-cluster-env-template.sh
@@ -3,78 +3,81 @@
 ##################################################################################################
 # This template can be used to tweak the environment variables needed to run the E2E tests locally #
 # Default behavior:
-# Runs test on an existing cluster in dev0-iad
+# Runs test on an existing cluster
+# Note: All variables that have comment as "# Mandatory" need to be filled with appropriate values for the tests to run correctly.
 
 # To run the tests:
-# 1. Change the FOCUS variable here to specify the subset of E2E tests to run
+# 1. Change the FOCUS valiable here to specify the subset of E2E tests to run
 # 2. Set CLUSTER_KUBECONFIG and CLOUD_CONFIG if needed
-# 3. run 'source existing-cluster-dev0-env-template.sh' to set the variables
+# 3. run 'source existing-standalone-cluster-env-template.sh' to set the variables
 # 4. run 'make run-ccm-e2e-tests-local`
 ##################################################################################################
 
 # The test suites to run (can replace or add tags)
-export FOCUS="\[test1\]"
+export FOCUS="\[cloudprovider\]"
 
 # The test suites to skip (can replace or add tags)
 export FOCUS_SKIP=""
 
-# Run E2Es in parallel. # of ginkgo "nodes" will be decided automatically based on CPU cores.
-export ENABLE_PARALLEL_RUN=true
+# Scope can be ARM / AMD / BOTH
+# Mandatory
+export SCOPE="BOTH"
 
-# This variable tells the test not to install oci cli and wipe out your .oci/config
-export LOCAL_RUN=1
-export TC_BUILD=0
+# A Reserved IP in your compartment for testing LB creation with Reserved IP
+# Create a public reserved IP in your compartment using the following link:
+# https://docs.oracle.com/en-us/iaas/Content/Network/Tasks/managingpublicIPs.htm#console-reserved
+# Set the public reserved IP in the following env-variable:
+# Mandatory
+export RESERVED_IP=""
 
-# This allows you to use your existing cluster
-export ENABLE_CREATE_CLUSTER=false
+# Set path to kubeconfig of existing cluster if it does not exist in default path. Defaults to $HOME/.kube/config.
+# Mandatory
+export CLUSTER_KUBECONFIG=$HOME/.kube/config
 
-# Set path to kubeconfig of existing cluster if it does not exist in default path. Defaults to $HOME/.kube/config_*
-export CLUSTER_KUBECONFIG=/Users/ypgohoka/.e2e_ccm_csi/oss-1-31.kubeconfig
+# Set path to cloud_config of existing cluster if it does not exist in default path. Defaults to $HOME/cloudconfig.
+# Mandatory
+export CLOUD_CONFIG=$HOME/cloudconfig
 
-# Set path to cloud_config of existing cluster if it does not exist in default path. Defaults to $HOME/cloudconfig_*
-export CLOUD_CONFIG=/Users/ypgohoka/go/src/github.com/oracle/oci-cloud-controller-manager/manifests/provider-config-example.yaml
+# ADLOCATION example is IqDk:US-ASHBURN-AD-1
+# Mandatory
+export ADLOCATION=""
 
+# KMS key for CMEK testing
+# CMEK KEY example "ocid1.key.relm.region.bb..cc.aaa...aa"
+# Mandatory
+export CMEK_KMS_KEY=""
 
-export IMAGE_PULL_REPO="iad.ocir.io/okedev/e2e-tests/"
-export ADLOCATION="Ddfp:US-ASHBURN-AD-2"
-
-#KMS key for CMEK testing
-export CMEK_KMS_KEY="ocid1.key.oc1.iad.b5r7iu5xaagy2.abuwcljsizwczmdfnitnxaruykluz3p6kwprasd2l7ebfvbkxbytvuumg4cq"
-
-#NSG Network security group created in cluster's VCN
-export NSG_OCIDS="ocid1.networksecuritygroup.oc1.iad.aaaaaaaarqaak4vupqsxits6crgsxu5p65eh7p422iry6qttvafn5jvhsmva,ocid1.networksecuritygroup.oc1.iad.aaaaaaaaie4b3quurf3b5sgwz7lwmygii65k3yvlhkyakqacu74xowe3763q"
+# NSG Network security group created in cluster's VCN
+# CCM E2E tests require two NSGs to run successfully. Please create two NSGs in the cluster's VCN and set NSG_OCIDS
+# NSG_OCIDS example ocid1.networksecuritygroup.relm.region.aa...aa,ocid1.networksecuritygroup.relm.region.aa...aa
+# Mandatory
+export NSG_OCIDS=","
 
 # NSG Network security group created in cluster's VCN for backend management, this NSG will have to be attached to the nodes manually for tests to pass
-export BACKEND_NSG_OCIDS="ocid1.networksecuritygroup.oc1.iad.aaaaaaaaie4b3quurf3b5sgwz7lwmygii65k3yvlhkyakqacu74xowe3763q"
+export BACKEND_NSG_OCIDS=""
 
-#Reserved IP created in e2e test compartment
-export RESERVED_IP="169.155.149.109"
-
-#Architecture to run tests on
-export ARCHITECTURE_AMD="AMD"
-export ARCHITECTURE_ARM="ARM"
-
-#Focus the tests : ARM, AMD or BOTH
-export SCOPE="AMD"
+# FSS VOLUME HANDLE in the format filesystem_ocid:mountTargetIP:export_path
+# Make sure fss volume handle is in the same subnet as your nodes
+# Create a file system, file export path and mount target in your VCN by following
+# https://docs.oracle.com/en-us/iaas/Content/File/Tasks/creatingfilesystems.htm#Using_the_Console
+# And setup your network for the file system by following:
+# https://docs.oracle.com/en-us/iaas/Content/File/Tasks/securitylistsfilestorage.htm
+# Mandatory
+export FSS_VOLUME_HANDLE=""
 
 # For debugging the tests in existing cluster, do not turn it off by default.
 # Optional
 # export DELETE_NAMESPACE=false
 
-# FSS volume handle
-# format is FileSystemOCID:serverIP:path
-export FSS_VOLUME_HANDLE="ocid1.filesystem.oc1.iad.aaaaaaaaaacdndlxnfqwillqojxwiotjmfsc2ylefuyqaaaa:10.0.73.199:/oss-test"
-export FSS_VOLUME_HANDLE_ARM="ocid1.filesystem.oc1.iad.aaaaaaaaaacdndlxnfqwillqojxwiotjmfsc2ylefuyqaaaa:10.0.73.199:/oss-test"
+# By default, public images are used. But if your Cluster's environment cannot access above public images then below option can be used to specify an accessible repo.
+# Optional
+# export IMAGE_PULL_REPO="accessiblerepo.com/repo/path/"
 
-export MNT_TARGET_ID="ocid1.mounttarget.oc1.iad.aaaaacvippzjdfiynfqwillqojxwiotjmfsc2ylefuyqaaaa"
-export MNT_TARGET_SUBNET_ID="ocid1.subnet.oc1.iad.aaaaaaaafujcpvwdn3s2liqwrilolm7jmxkwq35zieo7zk4medjtqxjac7cq"
-export MNT_TARGET_COMPARTMENT_ID="ocid1.compartment.oc1..aaaaaaaaee2fxlf36idmiqlyvnyhkh2oquz5loogbmzat73hnnqhu2c3352a"
+export MNT_TARGET_ID=""
+export MNT_TARGET_SUBNET_ID=""
+export MNT_TARGET_COMPARTMENT_ID=""
 
-export STATIC_SNAPSHOT_COMPARTMENT_ID="ocid1.compartment.oc1..aaaaaaaaee2fxlf36idmiqlyvnyhkh2oquz5loogbmzat73hnnqhu2c3352a"
-
-# For SKE node, node_info, node_lifecycle controller tests against PDE
-# To setup PDE and point your localhost:25000 to the PDE CP API refer: Refer: https://bitbucket.oci.oraclecorp.com/projects/OKE/repos/oke-control-plane/browse/personal-environments/README.md
-# export CE_ENDPOINT_OVERRIDE="http://localhost:25000"
+export STATIC_SNAPSHOT_COMPARTMENT_ID=""
 
 # Whether to run UHP E2Es or not, requires Volume Management Plugin enabled on the node and 16+ cores
 # Check the following doc for the exact requirements:
